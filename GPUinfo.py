f1dc5c76c3f4c8b0c3950c1ce1afb949c0bcb57f
TheOwner <theowner@toaster.thehome.com>
2026-02-17 21:01:33 +0900

    feat: Add function to parse through command string and return list of commands. Will return two lists if original command contains | operator.

--- GPUinfo.py
+++ GPUinfo.py
@@ -1,6 +1,10 @@
 import platform
 import subprocess
 
+import typing
+from typing import List
+from typing import Optional
+
 class OSCompatibiltyError(Exception):
   def __init__(self, message, os):
     super().__init__(message)
@@ -32,11 +36,18 @@ def cmd(device_os):
 
   return CMD_DICT.get(device_os)
 
-def process_cmd(cmd):
+def parse_cmd(cmd):
   """ 
   Divides command at the | operator and separates them into 2 lists. The two lists
   are then split at every white-space. This makes 2 lists of commands to be run
   """
   cmd_lists = [word.split() for word in cmd.split('|')]
-  
   assert len(cmd_lists) <= 2, "cmd_list contains too many lists of commands, Max Num of list: 2"
+
+  GPUname_getter_cmd1 = cmd_lists[0]
+
+  GPUname_getter_cmd2 = None
+  if len(cmd_lists) == 2:
+    GPUname_getter_cmd2 = cmd_lists[1]
+
+  return GPUname_getter_cmd1, GPUname_getter_cmd2
